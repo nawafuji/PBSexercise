69ad4240da7a3fac861f2fb63c716d0baee7c694
leylakern <lkern@student.ethz.ch>
2015-09-24 09:14:57 +0200

    Update Exercise.cpp

--- Ex1/Exercise.cpp
+++ Ex1/Exercise.cpp
@@ -8,18 +8,22 @@ static const double g = 9.81;
 // hanging mass point
 void AdvanceTimeStep1(double k, double m, double d, double L, double dt, int method, double p1, double v1, double& p2, double& v2)
 {
- double F=0.,k1=0.,k2=0.,l1=0.;
+  double F=0.,k1=0.,k2=0.,l1=0.;
     //Euler
     if (method==1)
     {
+        
+        F=-k*(abs(p2-p1)-L)*(p2-p1)/abs(p2-p1);        //not sure if this is correct
         p2 = p2+dt*v2;
-        F=-k*p2;        //not sure if this is correct
-        v2= v2+ dt*(F-v2*d)/m;
+        v2= v2+ dt*(F-m*g-v2*d)/m;
+       
+        
+        
     }else if(method==3) //midpoint
     {
         k1=v2;
-        F=-k*p2;
-        l1=(F-v2*d)/m;
+        F=-k*(abs(p2-p1)-L)*(p2-p1)/abs(p2-p1);     //not sure if this is correct
+        l1=(F-m*g-v2*d)/m;
         k2=v2+dt*l1;
         
         p2=p2+dt/2*(k1+k2);
